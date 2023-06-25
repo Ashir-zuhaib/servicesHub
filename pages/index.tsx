a5562bf3033c35ec4ad8df236c28535f2ac905f7
mahnoorGL <mahnoor.naveed@get-licensed.co.uk>
2023-06-25 20:16:25 +0500

    carousel and dummy images added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 import Container from "@mui/material/Container";
 import BrandCarousel from "../components/shared/BrandsCarousel/BrandCarousel";
 import ProductsContainerWithButton from "../components/shared/ProductsContainer/ProductsContainerWithButton";
+import { Example } from "../components/shared/Carousel/Carousel";
 
 const Home: any = () => {
   let isMobileValue = false;
@@ -9,6 +10,7 @@ const Home: any = () => {
     <>
       <Layout isMobile={isMobileValue}>
         <Container maxWidth="lg">
+          <Example/>
           <BrandCarousel isMobile={isMobileValue} />
           <ProductsContainerWithButton
             title={"All Services"}
