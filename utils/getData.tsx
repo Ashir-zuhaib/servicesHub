07af17f2e97f0074235bcccd296f30ff9068d18a
Ashir-zuhaib <aashir2027270@gmail.com>
2023-07-25 07:15:12 -0600

    getData

--- utils/getData.tsx
+++ utils/getData.tsx
@@ -12,5 +12,24 @@ const getAllService =async()=>{
     })
     return services
 }
+const getWorkerByService =async(role)=>{
+    console.log("rossle",role);
+    
+    let services = []
+    if(role){
+    await firebase.firestore().collection("Users").where("worker","==", true)
+    .where("role","==",role).get()
+    .then((querySnapshot)=>{
+        console.log("sixe", querySnapshot.size);
+        
+        querySnapshot.forEach((doc)=>{
+            let data = doc.data()
+            data.id = doc.id
+            services.push(data)
+        })
+    })
+}
+    return services
+}
 
-export {getAllService}
+export {getAllService, getWorkerByService}
