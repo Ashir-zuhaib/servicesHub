58b3737f47c230ab5d468e238bb6bd3379dece6e
mahnoorGL <mahnoor.naveed@get-licensed.co.uk>
2023-09-15 03:27:21 +0500

    fix loading issues'

--- components/BookingDetails/BookingDetails.tsx
+++ components/BookingDetails/BookingDetails.tsx
@@ -1,37 +1,29 @@
 import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import CardMedia from "@mui/material/CardMedia";
 import { getBookings, getService, getUser } from "../../utils/getData";
 import { BookingCard } from "./BookingCard";
 import swal from "sweetalert";
-
 import firebase from "../../config";
+import { CircularProgress } from "@mui/material";
+
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
 }
 
-
 function CustomTabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
-
   return (
     <div
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      {...other}>
+      {value === index && <Box sx={{ py: 2 }}>{children}</Box>}
     </div>
   );
 }
@@ -43,17 +35,19 @@ function a11yProps(index: number) {
   };
 }
 
-export default function BookingTabs({currentUser}) {
-  const [userId, setUSerId]= React.useState("")
+export default function BookingTabs({ currentUser }) {
+  const [userId, setUSerId] = React.useState("");
   const [value, setValue] = React.useState(0);
   const [bookingItem, setBookingItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true); // Added loading state
+
   const getBookingData = async (value) => {
-    setUSerId(currentUser)    
+    setUSerId(currentUser);
     let status = "active";
     status = value == 1 ? "completed" : value == 2 ? "cancelled" : "active";
 
-    let bookingData = await getBookings(status,currentUser );
-    // Fetch customerData and serviceProviderData for each booking item
+    let bookingData = await getBookings(status, currentUser);
+
     const updatedBookingData = await Promise.all(
       bookingData.map(async (item) => {
         const customerData = await getUser(item?.customerId);
@@ -69,58 +63,75 @@ export default function BookingTabs({currentUser}) {
       })
     );
     setBookingItems(updatedBookingData);
+    setIsLoading(false); // Data is loaded, set isLoading to false
   };
 
   React.useEffect(() => {
     getBookingData(value);
-
   }, [value]);
+
   const handleCompleteBooking = (bookingId) => {
-    // Update the bookingItem state to exclude the completed booking
     setBookingItems((prevBookingItems) =>
       prevBookingItems.filter((item) => item.id !== bookingId)
     );
-    firebase.firestore().collection("Bookings").doc(bookingId).update({
-      status:"completed"
-    }).then(()=>{
-      swal("Completed")
-    })
-    
+    firebase
+      .firestore()
+      .collection("Bookings")
+      .doc(bookingId)
+      .update({
+        status: "completed",
+      })
+      .then(() => {
+        swal("Completed");
+      });
   };
 
   const handleCancelBooking = (bookingId) => {
-    // Update the bookingItem state to exclude the canceled booking
     setBookingItems((prevBookingItems) =>
       prevBookingItems.filter((item) => item.id !== bookingId)
     );
-    firebase.firestore().collection("Bookings").doc(bookingId).update({
-      status:"cancelled"
-    }).then(()=>{
-      swal("Cancelled")
-    })
-    // Make an API call or perform any other necessary actions for canceling the booking
-    // ...
+    firebase
+      .firestore()
+      .collection("Bookings")
+      .doc(bookingId)
+      .update({
+        status: "cancelled",
+      })
+      .then(() => {
+        swal("Cancelled");
+      });
   };
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
+    setIsLoading(true); // Set isLoading to true when changing tabs to show loading indicator
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: "100%" }} className="px-0 pt-3">
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
         <Tabs
           value={value}
           onChange={handleChange}
-          aria-label="basic tabs example"
-        >
+          aria-label="basic tabs example">
           <Tab label="Active" {...a11yProps(0)} />
           <Tab label="Completed" {...a11yProps(1)} />
           <Tab label="Cancelled" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        {bookingItem.length ? (
+        {isLoading ? ( // Display loading indicator while data is loading
+          <Box
+            sx={{
+              height: "100vh",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}>
+            <CircularProgress />
+          </Box>
+        ) : bookingItem.length ? (
           bookingItem?.map((item) =>
             item?.status == "active" ? (
               <BookingCard
@@ -138,11 +149,26 @@ export default function BookingTabs({currentUser}) {
         )}
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-        {bookingItem.length ? (
+        {isLoading ? ( // Display loading indicator while data is loading
+          <Box
+            sx={{
+              height: "100vh",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}>
+            <CircularProgress />
+          </Box>
+        ) : bookingItem.length ? (
           bookingItem?.map((item) =>
             item?.status == "completed" ? (
-              <BookingCard key={item.id} item={item} onComplete={() => handleCompleteBooking(item.id)}
-              onCancel={() => handleCancelBooking(item.id)}  />
+              <BookingCard
+                key={item.id}
+                item={item}
+                onComplete={() => handleCompleteBooking(item.id)}
+                onCancel={() => handleCancelBooking(item.id)}
+              />
             ) : (
               ""
             )
@@ -152,11 +178,26 @@ export default function BookingTabs({currentUser}) {
         )}
       </CustomTabPanel>
       <CustomTabPanel value={value} index={2}>
-        {bookingItem.length ? (
+        {isLoading ? ( // Display loading indicator while data is loading
+          <Box
+            sx={{
+              height: "100vh",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}>
+            <CircularProgress />
+          </Box>
+        ) : bookingItem.length ? (
           bookingItem?.map((item) =>
             item?.status == "cancelled" ? (
-              <BookingCard key={item.id} item={item} onComplete={() => handleCompleteBooking(item.id)}
-              onCancel={() => handleCancelBooking(item.id)}/>
+              <BookingCard
+                key={item.id}
+                item={item}
+                onComplete={() => handleCompleteBooking(item.id)}
+                onCancel={() => handleCancelBooking(item.id)}
+              />
             ) : (
               ""
             )
--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,38 +1,43 @@
 import Layout from "../components/layout";
 import Container from "@mui/material/Container";
 import { useEffect, useState } from "react";
-// DUMMY IMAGES
-import bannerImage1 from "../public/images/banner-img.png";
-import bannerImage2 from "../public/images/banner-img2.png";
-import BookingTabs from "../components/BookingDetails/BookingDetails"
+import BookingTabs from "../components/BookingDetails/BookingDetails";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import websiteLogo from "../public/logo.png";
 
 const Dashboard: any = () => {
   const router = useRouter();
-
-  let isMobileValue = false;
   const [userId, setID] = useState("");
-  const getData = async()=>{
+  const getData = async () => {
     let currentUserId = await localStorage?.getItem("uid");
-    if(!currentUserId){
-      router.push("/")
+    if (!currentUserId) {
+      router.push("/");
     }
-    setID(currentUserId)
-  }
+    setID(currentUserId);
+  };
   // FETCH SERVICES DATA
- useEffect(()=>{
-  getData()
- },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
   // CAROUSEL IMAGES
-  
+
   return (
     <>
-      <Layout isMobile={isMobileValue}>
-        <Container maxWidth="lg">
-         <BookingTabs currentUser={userId}/> 
+      <div className="w-100 primary-color">
+        <Container maxWidth="lg" className="py-3">
+          <Image
+            src={websiteLogo}
+            alt="Logo"
+            width={180}
+            height={60}
+          />
         </Container>
-      </Layout>
+      </div>
+      <Container maxWidth="lg">
+        <BookingTabs currentUser={userId} />
+      </Container>
     </>
   );
 };
